b5b270b5e3a63fa20d5f64c7d6b4e8f9d06ba75a
Jaegoomon <jaegoo1199@gmail.com>
2020-12-30 13:42:31 +0900

    사진항목 추가, 레이아웃 일부수정

--- app/src/main/java/com/example/cs496_week1/fragments/PersonAdapter.kt
+++ app/src/main/java/com/example/cs496_week1/fragments/PersonAdapter.kt
@@ -3,42 +3,45 @@ package com.example.cs496_week1.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cs496_week1.R
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private val kode = arrayOf(
-        "d116df5",
-        "36ffc75", "f5cfe78", "5b87628",
-        "db8d14e", "9913dc4", "e120f96",
-        "466251b"
+    private val name = arrayOf(
+        "조재구",
+        "조재구", "조재구", "조재구",
+        "조재구", "9913dc4", "조재구",
+        "조재구"
     )
 
-    private val kategori = arrayOf(
-        "Kekayaan", "Teknologi",
-        "Keluarga", "Bisnis",
-        "Keluarga", "Hutang",
-        "Teknologi", "Pidana"
+    private val studentId = arrayOf(
+        "20170633", "20170633",
+        "20170633", "20170633",
+        "20170633", "20170633",
+        "20170633", "20170633"
     )
 
-    private val isi = arrayOf(
-        "pertanyaan 9",
-        "pertanyaan 11", "pertanyaan 17", "test forum",
-        "pertanyaan 12", "pertanyaan 18", "pertanyaan 20",
-        "pertanyaan 21"
+    private val number = arrayOf(
+        "010-9833-1299",
+        "010-9833-1299", "010-9833-1299", "010-9833-1299",
+        "010-9833-1299", "010-9833-1299", "010-9833-1299",
+        "010-9833-1299"
     )
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var itemKode: TextView
-        var itemKategori: TextView
-        var itemTsi: TextView
+        var itemName: TextView
+        var itemStudentId: TextView
+        var itemNumber: TextView
+        var itemPhoto: ImageView
 
         init {
-            itemKode = itemView.findViewById(R.id.kodePertanyaan)
-            itemKategori = itemView.findViewById(R.id.kategori)
-            itemTsi = itemView.findViewById(R.id.isiPertanyaan)
+            itemName = itemView.findViewById(R.id.name)
+            itemStudentId = itemView.findViewById(R.id.student_id)
+            itemNumber = itemView.findViewById(R.id.phone_number)
+            itemPhoto = itemView.findViewById(R.id.photo)
         }
     }
 
@@ -49,12 +52,13 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemKode.text = kode[position]
-        holder.itemKategori.text = kategori[position]
-        holder.itemTsi.text = isi[position]
+        holder.itemName.text = name[position]
+        holder.itemStudentId.text = studentId[position]
+        holder.itemNumber.text = number[position]
+        holder.itemPhoto.setImageResource(R.drawable.ic_baseline_person_24)
     }
 
     override fun getItemCount(): Int {
-        return kode.size
+        return name.size
     }
 }
