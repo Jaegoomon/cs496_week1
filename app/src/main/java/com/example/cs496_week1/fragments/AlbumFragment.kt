112b0eb94521668f7f37a185d95f8c870a4c0c49
10EastSea <xgf6580@gmail.com>
2020-12-30 15:58:26 +0900

    이미지 1차

--- app/src/main/java/com/example/cs496_week1/fragments/AlbumFragment.kt
+++ app/src/main/java/com/example/cs496_week1/fragments/AlbumFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cs496_week1.R
+import com.example.cs496_week1.fragments.adapters.RecyclerAdapterImage
 
 class AlbumFragment : Fragment() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapterImage.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,5 +25,13 @@ class AlbumFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_album, container, false)
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        itemView.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = GridLayoutManager(activity, 3)
+            adapter = RecyclerAdapterImage()
+        }
     }
 }
