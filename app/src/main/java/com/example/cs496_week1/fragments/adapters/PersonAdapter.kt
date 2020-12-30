3f94916284f15411d9bd3e0047a1cf6b42fe27b6
Jaegoomon <jaegoo1199@gmail.com>
2020-12-30 11:39:26 +0900

    리사이클러뷰 개요

---
+++ app/src/main/java/com/example/cs496_week1/fragments/adapters/PersonAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.cs496_week1.fragments.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cs496_week1.R
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private val kode = arrayOf(
+        "d116df5",
+        "36ffc75", "f5cfe78", "5b87628",
+        "db8d14e", "9913dc4", "e120f96",
+        "466251b"
+    )
+
+    private val kategori = arrayOf(
+        "Kekayaan", "Teknologi",
+        "Keluarga", "Bisnis",
+        "Keluarga", "Hutang",
+        "Teknologi", "Pidana"
+    )
+
+    private val isi = arrayOf(
+        "pertanyaan 9",
+        "pertanyaan 11", "pertanyaan 17", "test forum",
+        "pertanyaan 12", "pertanyaan 18", "pertanyaan 20",
+        "pertanyaan 21"
+    )
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemKode: TextView
+        var itemKategori: TextView
+        var itemTsi: TextView
+
+        init {
+            itemKode = itemView.findViewById(R.id.kodePertanyaan)
+            itemKategori = itemView.findViewById(R.id.kategori)
+            itemTsi = itemView.findViewById(R.id.isiPertanyaan)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemKode.text = kode[position]
+        holder.itemKategori.text = kategori[position]
+        holder.itemTsi.text = isi[position]
+    }
+
+    override fun getItemCount(): Int {
+        return kode.size
+    }
+}
