3b85ab9b9662dd362f306baedae6cc467bb72c1e
10EastSea <xgf6580@gmail.com>
2020-12-30 16:25:58 +0900

    이미지 2차(PhotoView 이용한 이미지 확대)

--- app/src/main/java/com/example/cs496_week1/fragments/FullView.kt
+++ app/src/main/java/com/example/cs496_week1/fragments/FullView.kt
@@ -6,13 +6,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import com.example.cs496_week1.R
+import com.github.chrisbanes.photoview.PhotoView
 
 class FullView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_view)
 
-        var fullImg: ImageView = findViewById(R.id.fullView)
+        var fullImg: PhotoView = findViewById(R.id.fullView)
         val intent: Intent = getIntent()
         val img = intent.getIntExtra("IMG", 0)
 
