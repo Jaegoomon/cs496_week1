36550abb5cb5c38eba729ea41d5ae22cfe459682
Jaegoomon <jaegoo1199@gmail.com>
2020-12-30 11:45:04 +0900

    리사이클러뷰 개요

--- app/src/main/java/com/example/cs496_week1/fragments/ContactFragment.kt
+++ app/src/main/java/com/example/cs496_week1/fragments/ContactFragment.kt
@@ -5,15 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.cs496_week1.R
-import com.example.cs496_week1.fragments.adapters.RecyclerAdapter
 
 class ContactFragment : Fragment() {
-
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,14 +18,5 @@ class ContactFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val recycler_view: RecyclerView = view.findViewById(R.id.recycler_view)
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(activity)
-            adapter = RecyclerAdapter()
-        }
     }
 }
