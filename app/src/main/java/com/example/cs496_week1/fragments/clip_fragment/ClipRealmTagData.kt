498ebad06e4aa5b6297602bf8170f41679441a41
Jaegoomon <jaegoo1199@gmail.com>
2021-01-04 10:29:49 +0900

    에러로인한 Tag 데이터 삭제

--- app/src/main/java/com/example/cs496_week1/fragments/clip_fragment/ClipRealmTagData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cs496_week1.fragments.clip_fragment
-
-import io.realm.RealmObject
-
-class ClipRealmTagData : RealmObject() {
-    var tag: String = ""
-    var color: String = ""
-}
--- app/src/main/java/com/example/cs496_week1/fragments/clip_fragment/RealmApplication.kt
+++ app/src/main/java/com/example/cs496_week1/fragments/clip_fragment/RealmApplication.kt
@@ -9,12 +9,7 @@ class RealmApplication : Application() {
         super.onCreate()
         Realm.init(this)
         val config1 =
-            RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().name("mydata.realm")
-                .modules(ClipRealmData()).build()
+            RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().name("mydata.realm").build()
         Realm.setDefaultConfiguration(config1)
-
-        val config2 =
-            RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().name("tagdata.realm")
-                .modules(ClipRealmTagData()).build()
     }
 }
